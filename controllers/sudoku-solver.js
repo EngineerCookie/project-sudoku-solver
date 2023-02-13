4b07c44956b4e58db618fe900ba67c9dffc8ea30
Tony <lam_273@hotmail.com>
2023-02-13 12:01:29 -0500

    check row, column, region  function working

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,18 +1,40 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    let validateRegex = /^[\d.]+$/;
+    if (!puzzleString) { return { error: 'Required field missing' }};
+    if (puzzleString.length !== 81) { return { error: 'Expected puzzle to be 81 characters long' }};
+    if (!validateRegex.test(puzzleString)) {return { error: 'Invalid characters in puzzle' }};
+    return 'valid'
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
-
+  checkRowPlacement(puzzleObj, row, column, value) {
+    return puzzleObj[row].includes(value)
   }
 
-  checkColPlacement(puzzleString, row, column, value) {
-
+  checkColPlacement(puzzleObj, row, column, value) {
+    let rowArr = Object.keys(puzzleObj);
+    let colArr = [];
+    rowArr.forEach(n => { colArr.push(puzzleObj[n][column - 1])})
+    return colArr.includes(value)
   }
 
-  checkRegionPlacement(puzzleString, row, column, value) {
+  checkRegionPlacement(puzzleObj, row, column, value) {
+    let rowGroup = [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i']];
+    let colGroup = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    let quadrant = {
+      rowQuad: rowGroup.find(arr => {return arr.includes(row)}),
+      colQuad: colGroup.find(arr => {return arr.includes(+column)})
+    };
+    let regionArr = [];
+    
+    quadrant.rowQuad.forEach(row => {
+      quadrant.colQuad.forEach(col => {
+        regionArr.push(puzzleObj[row][`${col-1}`])
+      });
+    });
 
+    return regionArr.includes(`${value}`) //This is cuz input value is in String format.
   }
 
   solve(puzzleString) {
--- routes/api.js
+++ routes/api.js
@@ -5,14 +5,65 @@ const SudokuSolver = require('../controllers/sudoku-solver.js');
 module.exports = function (app) {
   
   let solver = new SudokuSolver();
-
   app.route('/api/check')
     .post((req, res) => {
-
+      let validation = solver.validate(req.body.puzzle);
+      if (validation !== 'valid') {
+        return res.json(validation);
+      }
+      if (!req.body.coordinate || !req.body.value) {
+        return res.json({ "error": "Required field(s) missing" });
+      }
+      let puzzleObj = {
+        'a': req.body.puzzle.split("").slice(0, 9),
+        'b': req.body.puzzle.split("").slice(9, 18),
+        'c': req.body.puzzle.split("").slice(18, 27),
+        'd': req.body.puzzle.split("").slice(27, 36),
+        'e': req.body.puzzle.split("").slice(36, 45),
+        'f': req.body.puzzle.split("").slice(45, 54),
+        'g': req.body.puzzle.split("").slice(54, 63),
+        'h': req.body.puzzle.split("").slice(63, 72),
+        'i': req.body.puzzle.split("").slice(72, 81),
+      };
+      let checkResponse = {};
+      let args = [puzzleObj, ...req.body.coordinate.split(''), req.body.value];
+      args[1] = args[1].toLowerCase();  
+      if (solver.checkRowPlacement(...args) || solver.checkColPlacement(...args) || solver.checkRegionPlacement(...args)) {
+        checkResponse.valid = false;
+        checkResponse.conflict = [];
+        if(solver.checkRowPlacement(...args)){ checkResponse.conflict.push('row') };
+        if(solver.checkColPlacement(...args)){ checkResponse.conflict.push('column') };
+        if(solver.checkRegionPlacement(...args)){  checkResponse.conflict.push('region') };
+        return res.json(checkResponse)
+      } else { checkResponse.valid = true };
+      res.json(checkResponse);
     });
     
   app.route('/api/solve')
     .post((req, res) => {
 
     });
+
+  app.route('/test')
+  .get((req, res) => {
+    let puzzleObj = {};
+    let test = '  ';
+    let testString = '135762984946381257728459613694517832812936745357824196473298561581673429269145378'
+    let testString2 ='1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'
+    let testObj = {
+      a: testString.split("").slice(0, 9),
+      b: testString.split("").slice(9, 18),
+      c: testString.split("").slice(18, 27),
+      d: testString.split("").slice(27, 36),
+      e: testString.split("").slice(36, 45),
+      f: testString.split("").slice(45, 54),
+      g: testString.split("").slice(54, 63),
+      h: testString.split("").slice(63, 72),
+      i: testString.split("").slice(72, 81),
+    };
+    puzzleObj.arr = [];
+    puzzleObj.arr.push = 'Tupapi';
+    console.log(puzzleObj)
+    res.send('testing')
+  })
 };
